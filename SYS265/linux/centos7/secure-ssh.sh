4a359561c9993e53a49eba230b3208eaddb689bd
michael-D-S <michael.sargent@mymail.champlain.edu>
2025-02-15 16:29:32 -0500

    Add linux and Centos7-secure-ssh-sh

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author michael-D-S
+#creates a new ssh user using $1 parameters
+#adds a public key from the local repo or curled from the remote repo
+#removes root ability to the ssh in
+echo "ALL YOUR CODE GOES HERE"
