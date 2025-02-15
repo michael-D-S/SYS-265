a2cc5ef2953f0608e9428d021f00d34a8592b39d
michael-D-S <michael.sargent@mymail.champlain.edu>
2025-02-15 22:43:17 +0000

    Update secure-ssh.sh with passwordless login automation

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,26 @@
 #creates a new ssh user using $1 parameters
 #adds a public key from the local repo or curled from the remote repo
 #removes root ability to the ssh in
-echo "ALL YOUR CODE GOES HERE"
+#!/bin/bash
+
+USERNAME=$1
+
+if [ -z "$USERNAME" ]; then
+ echo "Usage: ./secure-ssh.sh <username>"
+ exit 1
+fi
+
+#create the user
+sudo adduser $USERNAME
+
+#Creat the .ssh directory
+sudo mkdir /home/$USERNAME/.ssh
+
+#append the public key to authroized_key
+sudo cat ssh/web01_id_rsa.pub >> /home/$USERNAME/.ssh/authorized_keys
+
+#Set correct permissions
+sudo chown $USERNAME:$USERNAME /home/$USERNAME/.ssh/authorized_keys
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+
+echo "User $USERNAME configuration for passwordless SSH login."
